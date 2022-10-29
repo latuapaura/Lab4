9f76aeca7198124767a3e6c2469c70856092dd4f
latuapaura <yanamigliore@gmail.com>
2022-10-29 17:11:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+data = {
+    'tuesday':
+        [
+            {
+                'time': '10:00',
+                'address': 'St.Ptersburg',
+                'class': 123,
+                'discipline': 'Programmer'
+            },
+            {
+                'time': '12:00',
+                'address': 'St.Ptersburg',
+                'class': 4523,
+                'discipline': 'Biology'
+            },
+            {
+                'time': '14:00',
+                'address': 'St.Ptersburg',
+                'class': 32,
+                'discipline': 'Programming'
+            }
+        ]
+}
